80b3e3d5fe8abe3c6a1ace949891d529dc95b1c6
leamrossb-afk <leamrossb@gmail.com>
2026-06-05 17:57:17 +0000

    Task2.2

---
+++ BoloconTask2.java
@@ -0,0 +1,12 @@
+public class BoloconTask2 {
+        public static void main(String[] args) {
+        char h = 'H', w = 'w', r = 'r', d = 'd';
+        int ello = 3110;
+        byte o = 0;
+        short l = 1;
+        float f = 2.0f;
+        boolean b = true;
+        String output = "" + h + ello + " " + w + o + r + l + d + " " + f + " " + b; 
+        System.out.println(output);
+        }
+}
--- Main.java
+++ Main.java
@@ -5,15 +5,3 @@ public class Main{
         System.out.println("Hello World3"); // From codespace again.
     }
 }
-class Test {
-    public static void main(String[] args) {
-        char h = 'H', w = 'w', r = 'r', d = 'd';
-        int ello = 3110;
-        byte o = 0;
-        short l = 1;
-        float f = 2.0f;
-        boolean b = true;
-        String output = "" + h + ello + " " + w + o + r + l + d + " " + f + " " + b; 
-        System.out.println(output);
-}
-}
